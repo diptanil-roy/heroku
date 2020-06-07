11b34538794b63b190f381cf39e6c1860f6c25af
Diptanil Roy <roydiptanil@gmail.com>
2020-06-06 22:31:20 -0400

    Top 3 choices.

--- processing.py
+++ processing.py
@@ -2,6 +2,7 @@ import pickle
 import streamlit as stream
 import pandas as pd
 from src.model import LexicalFeatures, BOWFeatures, SyntacticFeatures, NGramFeatures
+from src.book import BookText
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
@@ -13,15 +14,31 @@ from sklearn.preprocessing import StandardScaler, FunctionTransformer, LabelEnco
 
 @stream.cache
 
+# def guess_author(snip: str):
+
+# 	book = BookText(rawtext=snip)
+# 	number_of_sentences = len(book.tokenize('sent', rem_stopwords = False))
+# 	model = pickle.load(open('model_fit','rb'))
+# 	author_list = ['Jane Austen', 'Lewis Carroll', 'Daniel Defoe', 'Charles Dickens', 'Sir Arthur Conan Doyle', 'Jack London', 'Joanne Rowling', 'Mary Shelley',
+#  'Louis Stevenson', 'Mark Twain', 'H.G. Wells', 'Oscar Wilde']
+# 	y = int(model.predict(snip))
+# 	z = round(model.predict_proba(snip)[0, y]*100, 1)
+
+# 	return author_list[y], z, number_of_sentences
+
+
 def guess_author(snip: str):
 
+	book = BookText(rawtext=snip)
+	number_of_sentences = len(book.tokenize('sent', rem_stopwords = False))
 	model = pickle.load(open('model_fit','rb'))
-	author_list = ['Jane Austen', 'Lewis Carroll', 'Daniel Defoe', 'Charles Dickens', 'Sir Arthur Conan Doyle', 'Jack London', 'Joanne Rowling', 'Mary Shelley',
- 'Louis Stevenson', 'Mark Twain', 'H.G. Wells', 'Oscar Wilde']
-	y = int(model.predict(snip))
-	z = round(model.predict_proba(snip)[0, y]*100, 1)
-
-	return author_list[y], z
+	author_list = np.array(['Jane Austen', 'Lewis Carroll', 'Daniel Defoe', 'Charles Dickens', 'Sir Arthur Conan Doyle', 'Jack London', 'Joanne Rowling', 'Mary Shelley',
+ 'Louis Stevenson', 'Mark Twain', 'H.G. Wells', 'Oscar Wilde'])
+	proba = np.reshape(model.predict_proba(snip), (len(author_list)))
+	three_highest_proba_ind = np.argpartition(proba, -3)[-3:]
+	pred_top_three = author_list[three_highest_proba_ind]
+	conf_top_three = proba[three_highest_proba_ind]
+	return pred_top_three, conf_top_three, number_of_sentences
 
 stream.title("Author Identification")
 stream.header("By team data-dart")
@@ -44,8 +61,13 @@ submit = stream.button("Guess Author")
 
 if submit:
 	if (snippet!= ""):
-		stream.write(guess_author(snippet)[0], "*with probability* ", guess_author(snippet)[1], "%")
-		# stream.write()
+		prob_author, confidence, num_sent =  guess_author(snippet)
+		if (num_sent < 25):
+			stream.write("*There are too few sentences. The predictions might be grossly incorrect. Please add a few more sentences.*")
+
+		stream.write(prob_author[2], "*with probability* ", round(confidence[2]*100,1), "%")
+		stream.write(prob_author[1], "*with probability* ", round(confidence[1]*100,1), "%")
+		stream.write(prob_author[0], "*with probability* ", round(confidence[0]*100,1), "%")
 
 	else:
 		stream.write("__*You must enter some text.*__")
